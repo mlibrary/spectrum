d03ae42195cf27e0f9964cd67a8ddee5f6c396be
Albert Bertram <bertrama@umich.edu>
2021-01-07 10:48:22 -0500

    ERB in YAML fix.

--- lib/keycard/cookie/institution_finder.rb
+++ lib/keycard/cookie/institution_finder.rb
@@ -31,7 +31,7 @@ module Keycard
       DEFAULT_KEY = 'dlpsInstitutionId'
 
       def initialize(config: 'config/keycard.yml')
-        @config = configure(YAML.load_file(config)['cookie'])
+        @config = configure(YAML.load(ERB.new(File.read(config)).resolve)['cookie'])
       end
 
       def attributes_for(request)
