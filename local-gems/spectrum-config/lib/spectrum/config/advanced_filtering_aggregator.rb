6738cf060328505aedb52e5a61cfe31db9a07bb2
Heymo <hvehse@umich.edu>
2022-09-30 10:30:00 -0400

    Looping for multiple new/previous titles

--- local-gems/spectrum-config/lib/spectrum/config/advanced_filtering_aggregator.rb
+++ local-gems/spectrum-config/lib/spectrum/config/advanced_filtering_aggregator.rb
@@ -12,52 +12,48 @@ module Spectrum
       end
 
       def to_value
-        [
+        @ret.values.map do |labelled_fields|
           [
             {
               uid: "display",
               name: "NAME",
-              value: to_display_value,
+              value: to_display_value(labelled_fields),
               value_has_html: true
             },
             {
               uid: "search",
               name: "NAME",
-              value: to_search_value,
+              value: to_search_value(labelled_fields),
               value_has_html: true
             }
           ]
-        ]
+        end
       end
 
-      def to_display_value
-        @ret.values.map do |labelled_fields|
-          labelled_fields.map do |joined_fields|
-            join = joined_fields[1].keys.first
-            values = joined_fields[1].values.first
-            if join
-              values.join(join)
-            else
-              values
-            end
+      def to_display_value(labelled_fields)
+        labelled_fields.map do |joined_fields|
+          join = joined_fields[1].keys.first
+          values = joined_fields[1].values.first
+          if join
+            values.join(join)
+          else
+            values
           end
         end.flatten.join(" ")
       end
 
-      def to_search_value
-        @ret.values.map do |labelled_fields|
-          labelled_fields.map do |joined_fields|
-            join = joined_fields[1].keys.first
-            values = joined_fields[1].values.first
-            joined_fields[1] = if join
-                                 values.join(join)
-                               else
-                                 values
-                               end
-            joined_fields[1].map do |value|
-              "#{joined_fields[0]}:#{quote(value)}"
-            end.join(" AND ")
-          end
+      def to_search_value(labelled_fields)
+        labelled_fields.map do |joined_fields|
+          join = joined_fields[1].keys.first
+          values = joined_fields[1].values.first
+          joined_fields[1] = if join
+                               values.join(join)
+                             else
+                               values
+                             end
+          joined_fields[1].map do |value|
+            "#{joined_fields[0]}:#{quote(value)}"
+          end.join(" AND ")
         end.flatten.join(" AND ")
       end
 
