0b32951ffdd1e7363a047517770b0a36f8bce144
Monique Rio <mrio@umich.edu>
2023-06-19 17:12:54 -0400

    feat: Alma Digital as its own kind of thing
    Solr will produce a holding with the library ALMA_DIGITAL for Alma
    Digital items. These items will have the fields described in the
    Spectrum::BibRecord::DigitalHolding class. They are presented as if they
    are Electronic Holdings so are a flavor of the ElectronicItemPresenter.

--- local-gems/spectrum-json/lib/spectrum/presenters/electronic_item_presenter.rb
+++ local-gems/spectrum-json/lib/spectrum/presenters/electronic_item_presenter.rb
@@ -3,13 +3,17 @@ module Spectrum
     def initialize(item)
       @item = item
     end
+
     def self.for(item)
-      if item.status == 'Not Available'
+      if item.library == "ALMA_DIGITAL"
+        DigitalItem.new(item)
+      elsif item.status == "Not Available"
         UnavailableElectronicItem.new(item)
       else
-        self.new(item)
+        new(item)
       end
     end
+
     def to_a
       [
         link,
@@ -17,24 +21,60 @@ module Spectrum
         {text: note}
       ]
     end
+
     def description
-      @item.description || ''
+      @item.description || ""
     end
+
     def note
-      @item.note || 'N/A'
+      @item.note || "N/A"
     end
+
     def link_text
-      @item.link_text || 'Available online'
+      @item.link_text || "Available online"
     end
+
     def link
-      { text: link_text, href: @item.link}
+      {text: link_text, href: @item.link}
     end
+
     class UnavailableElectronicItem < self
       def link
-        { text: 'Coming soon.' }
+        {text: "Coming soon."}
       end
+
       def description
-        [ 'Link will update when access is available.', @item.description || ''].join(' ').strip 
+        ["Link will update when access is available.", @item.description || ""].join(" ").strip
+      end
+    end
+
+    class DigitalItem
+      def initialize(item)
+        @item = item
+      end
+
+      def source
+        [@item.delivery_description, @item.public_note].compact.join("; ")
+      end
+
+      def link_text
+        @item.link_text || "Available online"
+      end
+
+      def link
+        {text: link_text, href: @item.link}
+      end
+
+      def label
+        @item.label || ""
+      end
+
+      def to_a
+        [
+          link,
+          {text: label},
+          {text: source}
+        ]
       end
     end
   end
--- spec/spectrum/presenters/electronic_item_presenter_spec.rb
+++ spec/spectrum/presenters/electronic_item_presenter_spec.rb
@@ -1,28 +1,65 @@
-require_relative '../../rails_helper.rb'
+require_relative "../../rails_helper"
 
-describe  Spectrum::Presenters::ElectronicItem do
-  before(:each) do
-    @item = double(Spectrum::BibRecord::ElectronicHolding, link: 'Link', link_text: 'Link Text', description: 'Description', note: 'Note', status: 'Available')
+describe Spectrum::Presenters::ElectronicItem do
+  context "E56 or 856 electronic item" do
+    before(:each) do
+      @item = instance_double(Spectrum::BibRecord::ElectronicHolding, link: "Link", link_text: "Link Text", description: "Description", note: "Note", status: "Available", library: "ELEC")
+    end
+    subject do
+      described_class.for(@item)
+    end
+    it "returns a regular electronic item when status is 'Available'" do
+      expect(subject.class).to eq(described_class)
+    end
+    it "returns unavailble electronic item when status is 'Not Available'" do
+      allow(@item).to receive(:status).and_return("Not Available")
+      expect(subject.class).to eq(Spectrum::Presenters::ElectronicItem::UnavailableElectronicItem)
+    end
+    it "has appropriate available item #to_a" do
+      expect(subject.to_a).to eq([{text: "Link Text", href: "Link"}, {text: "Description"}, {text: "Note"}])
+    end
+    it "has appropriate unavailable item #to_a" do
+      allow(@item).to receive(:status).and_return("Not Available")
+      expect(subject.to_a).to eq(
+        [
+          {text: "Coming soon."},
+          {text: "Link will update when access is available. Description"},
+          {text: "Note"}
+        ]
+      )
+    end
   end
-  subject do
-    described_class.for(@item)
-  end
-  it "returns a regular electronic item is status is 'Available'" do
-    expect(subject.class).to eq(described_class)
-  end
-  it "returns unavailble electronic item when status is 'Not Available'" do
-    allow(@item).to receive(:status).and_return('Not Available')
-    expect(subject.class).to eq(Spectrum::Presenters::ElectronicItem::UnavailableElectronicItem)
-  end
-  it "has appropriate available item #to_a" do
-    expect(subject.to_a).to eq([{text: 'Link Text', href: 'Link'},{text: 'Description'},{text: 'Note'}])
-  end
-  it "has appropriate unavailable item #to_a" do
-    allow(@item).to receive(:status).and_return('Not Available')
-    expect(subject.to_a).to eq(
-      [
-        {text: 'Coming soon.'},
-        {text: 'Link will update when access is available. Description'},
-        {text: 'Note'}])
+  context "Alma Digital electronic item" do
+    before(:each) do
+      @item = instance_double(Spectrum::BibRecord::DigitalHolding, link: "Link", link_text: "Link text", library: "ALMA_DIGITAL", public_note: "Public note", delivery_description: "Delivery description", label: "Label")
+    end
+    subject do
+      described_class.for(@item)
+    end
+    it "returns a digital electronic item present when library is ALMA Digital" do
+      expect(subject.class).to eq(Spectrum::Presenters::ElectronicItem::DigitalItem)
+    end
+    it "has appropriate digital item #to_a" do
+      expect(subject.to_a).to eq([
+        {text: "Link text", href: "Link"},
+        {text: "Label"},
+        {text: "Delivery description; Public note"}
+      ])
+    end
+    context "source" do
+      it "handles empty public note" do
+        allow(@item).to receive(:public_note).and_return(nil)
+        expect(subject.source).to eq("Delivery description")
+      end
+      it "handles nil delivery_description" do
+        allow(@item).to receive(:delivery_description).and_return(nil)
+        expect(subject.source).to eq("Public note")
+      end
+      it "handles both nil public note and delivery description" do
+        allow(@item).to receive(:public_note).and_return(nil)
+        allow(@item).to receive(:delivery_description).and_return(nil)
+        expect(subject.source).to eq("")
+      end
+    end
   end
 end
