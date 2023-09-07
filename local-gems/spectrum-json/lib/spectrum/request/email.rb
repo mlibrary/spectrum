19182735b1662fa9bf34de984d55010be4deb2ec
Albert Bertram <bertrama@umich.edu>
2023-09-07 12:26:21 -0400

    Updates for the new authentication setup.

--- local-gems/spectrum-json/lib/spectrum/request/email.rb
+++ local-gems/spectrum-json/lib/spectrum/request/email.rb
@@ -3,10 +3,10 @@
 module Spectrum
   module Request
     class Email
-      FLINT = 'Flint'
-      FLINT_PROXY_PREFIX = 'http://libproxy.umflint.edu:2048/login?url='
-      DEFAULT_PROXY_PREFIX = 'https://proxy.lib.umich.edu/login?url='
-      INSTITUTION_KEY = 'dlpsInstitutionId'
+      FLINT = "Flint"
+      FLINT_PROXY_PREFIX = "http://libproxy.umflint.edu:2048/login?url="
+      DEFAULT_PROXY_PREFIX = "https://proxy.lib.umich.edu/login?url="
+      INSTITUTION_KEY = "dlpsInstitutionId"
 
       def proxy_prefix
         return FLINT_PROXY_PREFIX if @request.env[INSTITUTION_KEY]&.include?(FLINT)
@@ -14,26 +14,26 @@ module Spectrum
       end
 
       attr_reader :role, :request
-      def initialize(request)
+      def initialize(request:, username:)
         @request = request
         @raw = CGI.unescape(request.raw_post)
         @data = JSON.parse(@raw)
-        @username = request.env['HTTP_X_REMOTE_USER'] || ''
-        @role = if request.env['dlpsInstitutionId'] &&
-            request.env['dlpsInstitutionId'].length > 0
-          'authenticated'
+        @username = username || ""
+        @role = if request.env["dlpsInstitutionId"] &&
+            request.env["dlpsInstitutionId"].length > 0
+          "authenticated"
         else
-          ''
+          ""
         end
         @items = nil
       end
 
       def to
-        @data['to']
+        @data["to"]
       end
 
       def from
-        return @username if @username.include?('@')
+        return @username if @username.include?("@")
         "#{@username}@umich.edu"
       end
 
@@ -50,11 +50,11 @@ module Spectrum
         @data.each_pair do |focus_uid, data|
           focus = Spectrum::Json.foci[focus_uid]
           next unless focus
-          data['records'].each do |id|
+          data["records"].each do |id|
             record = focus.fetch_record(Spectrum::Json.sources, id, role, self)
             yield record + [
-              { uid: 'base_url', value: data['base_url'] },
-              { uid: 'holdings', value: get_holdings(focus, id) }
+              { uid: "base_url", value: data["base_url"] },
+              { uid: "holdings", value: get_holdings(focus, id) }
             ]
           end
         end
