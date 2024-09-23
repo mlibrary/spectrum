5be1acd19f1eba4f9ac45d57882f83e9f15dd15e
Albert Bertram <bertrama@umich.edu>
2024-09-23 11:04:26 -0400

    Removed vestigial Rails code.

--- app/controllers/admin_controller.rb
+++
@@ -1,8 +0,0 @@
-# The beginnings of an administrative interface for managing
-# the Systems side of the application.
-#
-# 10/13 - this class is unused
-#
-class AdminController < ApplicationController
-  layout 'no_sidebar_no_search'
-end
