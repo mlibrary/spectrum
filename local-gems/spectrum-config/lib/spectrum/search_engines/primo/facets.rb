5e1a6cffb446043ba24b2345492fc41f4bdccb8a
Albert Bertram <bertrama@umich.edu>
2025-03-14 08:05:07 -0400

    One more unhandled exception.

--- local-gems/spectrum-config/lib/spectrum/search_engines/primo/facets.rb
+++ local-gems/spectrum-config/lib/spectrum/search_engines/primo/facets.rb
@@ -3,6 +3,7 @@ module Spectrum
     module Primo
       module Facets
         def self.for_json(json)
+          return [] unless json
           json.map {|data| Facet.for_json(data) }
         end
       end
