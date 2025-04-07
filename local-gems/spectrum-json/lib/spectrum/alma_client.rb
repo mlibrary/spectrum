06266a2bbe7c4c55604346ba040c5e0ebeb5ad92
Monique Rio <mrio@umich.edu>
2025-04-07 15:36:56 -0400

    adds httparty_on flag
    this is so this will hopefully still work even when we switch to v2 of
    the client

--- local-gems/spectrum-json/lib/spectrum/alma_client.rb
+++ local-gems/spectrum-json/lib/spectrum/alma_client.rb
@@ -1,22 +1,46 @@
 require "alma_rest_client"
 class Spectrum::AlmaClient
-  def self.client(http_party_on: false)
-    new(http_party_on: http_party_on)
+  def self.client(httparty_on: false)
+    new(httparty_on: httparty_on)
   end
   attr_reader :client
-  def initialize(http_party_on: false)
+  def initialize(httparty_on: false)
     @client = AlmaRestClient.client
+    @httparty_on = httparty_on
   end
 
   def get(path, kwargs = {})
-    @client.get(path, **kwargs)
+    response = @client.get(path, **kwargs)
+    if return_httparty?
+      httparty_response(response)
+    else
+      response
+    end
   end
 
   def post(path, kwargs = {})
-    @client.post(path, **kwargs)
+    response = @client.post(path, **kwargs)
+    if return_httparty?
+      httparty_response(response)
+    else
+      response
+    end
   end
 
   def get_all(kwargs)
-    @client.get_all(**kwargs)
+    response = @client.get_all(**kwargs)
+    if return_httparty?
+      httparty_response(response)
+    else
+      response
+    end
+  end
+
+  def return_httparty?
+    @httparty_on && response.class.to_s == "Faraday::Response"
+  end
+
+  def httparty_response(response)
+    Ostruct.new(code: response.status, parsed_response: response.body)
   end
 end
