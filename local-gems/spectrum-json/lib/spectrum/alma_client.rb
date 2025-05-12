c4cbdbefac3a8352cf0bea0ba557964cd9ae92ff
Monique Rio <mrio@umich.edu>
2025-05-12 16:15:46 -0400

    update alma_rest_client; turn off persistence

--- local-gems/spectrum-json/lib/spectrum/alma_client.rb
+++ local-gems/spectrum-json/lib/spectrum/alma_client.rb
@@ -1,5 +1,8 @@
 require "alma_rest_client"
 class Spectrum::AlmaClient
+  AlmaRestClient.configure do |config|
+    config.http_adapter = [:httpx, {persistent: false}]
+  end
   def self.client
     AlmaRestClient.client
   end
