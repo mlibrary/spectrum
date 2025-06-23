d45e2a42aaaf9eb97545cbdeeb933627b7e5d197
Albert Bertram <bertrama@umich.edu>
2025-06-23 15:29:07 -0400

    LIBSEARCH-1090 Add corporate authors where missing
    Corporate authors were missing from:
    * Catalog citations
    * Articles records and citations

---
+++ local-gems/spectrum-config/lib/spectrum/config/csl_corporate_author.rb
@@ -0,0 +1,13 @@
+module Spectrum
+  module Config
+    class CSLCorporateAuthor < CSLBase
+      def value(data)
+        if data && data[:value]
+          {id => [data[:value]].flatten.map {|name| { literal: name }}}
+        else
+          {}
+        end
+      end
+    end
+  end
+end
